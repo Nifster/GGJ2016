46055fde6cb213e61ee9faebcccf18e83040a032
Ohohcakester <ohohgit@gmail.com>
2016-01-30 00:30:07 +0800

    CharacterState Job API

--- GGJ2016/Assets/Scripts/CharacterAI.cs
+++ GGJ2016/Assets/Scripts/CharacterAI.cs
@@ -69,5 +69,7 @@ public class CharacterAI {
 
 #region Update Functions
 
+
+
 #endregion
 }
--- GGJ2016/Assets/Scripts/CharacterState.cs
+++ GGJ2016/Assets/Scripts/CharacterState.cs
@@ -1,13 +1,36 @@
 using UnityEngine;
 using System;
 
-public class CharacterState {
-
+public class CharacterState
+{
     public State state;
 
     public State currentJobState;
+    public float jobStartTime;
     public float jobFinishTime;
 
+    public void UpdateCurrentJobState(State s, float taskTime)
+    {
+        if (currentJobState == s)
+        {
+            // Continue Job.
+            return;
+        }
+        else
+        {
+            // Start Job.
+            currentJobState = s;
+            jobStartTime = Time.time;
+            jobFinishTime = jobStartTime + taskTime;
+        }
+    }
 
+    public float ProgressPercent
+    {
+        get { return Math.Min(1f, (Time.time - jobStartTime)/(jobFinishTime - jobStartTime)); }
+    }
 
+    public bool JobDone {
+        get { return Time.time > jobFinishTime; }
+    }
 }
