32ec1e5adb46787be7038cb4509033687a912484
Ohohcakester <ohohgit@gmail.com>
2016-01-30 12:41:08 +0800

    More enums

--- GGJ2016/Assets/Scripts/ActionTypes.cs
+++ GGJ2016/Assets/Scripts/ActionTypes.cs
@@ -2,11 +2,28 @@ using UnityEngine;
 using System;
 
 public enum DecisionType {
+    // The regular sequence of events.
     None,
     GoToilet,
     BrushTeeth,
-    MakeBed,
+    GetMilk,
+    PutMilkOnDiningTable,
+    GetCereal,
+    GetBowl,
+    MakeCereal,
+    EatCereal,
+    StartMakingCoffee,
+    GetClothes,
+    ChangeClothes,
+    GetNewspaper,
+    GetCoffee,
+    SitOnCouch,
+    GetKeysAndWallet,
+    GetBriefcase,
+    GetShoes,
+    LeaveHouse,
 
+    MakeBed,
 }
 
 
@@ -21,6 +38,22 @@ public enum JobType
     NONE,
     GO_TOILET,
     BRUSH_TEETH,
-    MAKE_BED,
+    GET_MILK,
+    PUT_MILK_ON_DINING_TABLE,
+    GET_CEREAL,
+    GET_BOWL,
+    MAKE_CEREAL,
+    EAT_CEREAL,
+    START_MAKING_COFFEE,
+    GET_CLOTHES,
+    CHANGE_CLOTHES,
+    GET_NEWSPAPER,
+    GET_COFFEE,
+    SIT_ON_COUCH,
+    GET_KEYS_AND_WALLET,
+    GET_BRIEFCASE,
+    GET_SHOES,
+    LEAVE_HOUSE,
 
+    MAKE_BED,
 }
