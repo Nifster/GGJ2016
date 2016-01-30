fa2044b1ca890e11d31020746dae002a39f13265
nifster <h_ghazali@hotmail.com>
2016-01-30 20:24:36 +0800

    Setup first iteration of actual scene

--- GGJ2016/Assets/Scripts/GameManager.cs
+++ GGJ2016/Assets/Scripts/GameManager.cs
@@ -20,6 +20,12 @@ public class GameManager : MonoBehaviour
 
     [SerializeField]
 	float realMinX, realMinY, realMaxX, realMaxY;
+
+	[SerializeField]
+	TextAsset floorPlan;
+
+	[SerializeField]
+	GameObject floorPrefab;
 	// Use this for initialization
 
 	public Dictionary<Point,Interactable> interactableHash = new Dictionary<Point,Interactable>();
@@ -42,38 +48,48 @@ public class GameManager : MonoBehaviour
 		//initialise pickupables
 		pickUpsHash.Add(new Point{x=2, y=2}, new PickUpable(PickUps.Bowl));
 
-        //initialize empty room
-        bool[,] isBlocked = new bool[10, 10];
-        for (int i = 0; i < isBlocked.GetLength(0); i++)
-        {
-            for (int j = 0; j < isBlocked.GetLength(1); j++)
-            {
-                isBlocked[i, j] = false;
-            }
-        }
-
         //set interactable in positions
 
-        isBlocked[0, 8] = true;
-        isBlocked[0, 4] = true;
-        isBlocked[1, 4] = true;
-        isBlocked[2, 4] = true;
-        isBlocked[3, 4] = true;
-        isBlocked[4, 4] = true;
-        isBlocked[0, 5] = true;
-        isBlocked[1, 5] = true;
-        isBlocked[2, 5] = true;
-        isBlocked[3, 5] = true;
-        isBlocked[4, 5] = true;
-        isBlocked[0, 6] = true;
-        isBlocked[1, 6] = true;
-        isBlocked[2, 6] = true;
-        isBlocked[3, 6] = true;
-        isBlocked[4, 6] = true;
-        isBlocked[4, 8] = true;
-        isBlocked[4, 9] = true;
+		var testBool = new int[23,20] {
+			{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+			{1,1,1,1,1,1,0,0,0,1,1,1,1,0,0,0,1,1,1,1},
+			{1,1,1,1,1,1,0,0,0,1,1,1,1,0,0,0,0,1,1,1},
+			{1,1,1,1,1,1,0,0,0,1,1,1,1,0,0,0,0,1,1,1},
+			{1,1,1,1,1,1,0,0,0,1,1,1,0,0,0,0,0,1,1,1},
+			{1,1,1,1,1,1,0,0,0,1,1,1,0,0,0,0,0,1,1,1},
+			{1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,1,1,1},
+			{1,1,1,1,1,1,0,0,0,1,1,1,0,0,0,0,0,1,1,1},
+			{1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1},
+			{1,1,1,1,0,0,1,1,1,0,0,0,0,0,0,0,0,1,1,1},
+			{1,1,1,1,0,0,1,1,1,0,0,0,0,0,0,0,0,1,1,1},
+			{1,1,1,1,0,0,1,1,1,0,0,0,0,0,0,0,0,1,1,1},
+			{1,1,1,1,0,0,1,1,1,0,0,0,0,0,0,0,0,1,1,1},
+			{1,1,1,1,0,0,1,1,1,0,0,0,0,0,0,0,0,1,1,1},
+			{1,1,1,1,0,0,1,1,1,0,0,0,0,0,0,0,0,1,1,1},
+			{1,1,1,1,0,0,1,1,1,0,0,0,0,0,0,0,0,1,1,1},
+			{1,1,1,1,0,0,1,1,1,0,0,0,0,0,0,0,0,1,1,1},
+			{1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1},
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1},
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,1,1,1},
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,1,1,1},
+			{1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1},
+			{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
 
-        houseGrid.Initialise(isBlocked, realMinX, realMinY, realMaxX - realMinX, realMaxY - realMinY);
+		};
+
+		var initBool = new bool[23,20];
+		for(int i = 0; i<23;i++){
+			for(int j=0; j<20;j++){
+				if(testBool[i,j] == 1){
+					initBool[i,j] = true;
+				}else{
+					initBool[i,j] = false;
+				}
+			}
+		}
+			
+		houseGrid.Initialise(initBool, realMinX, realMinY, realMaxX - realMinX, realMaxY - realMinY);
+
     }
 
     private void OnDrawGizmos()
@@ -85,7 +101,7 @@ public class GameManager : MonoBehaviour
         Gizmos.DrawLine(new Vector3(realMaxX, realMaxY, 0),
             new Vector3(realMinX, realMaxY, 0));
         Gizmos.DrawLine(new Vector3(realMaxX, realMaxY, 0),
-            new Vector3(realMaxX, realMinY, 0));
+			new Vector3(realMaxX, realMinY, 0));
 
 
         /*if (houseGrid != null)
@@ -111,14 +127,13 @@ public class GameManager : MonoBehaviour
 
     }
 
-    private void GizmosDrawTile(int px, int py, Color col)
-    {
-        if (houseGrid == null) return;
-        var vector = Vector2.zero;
-        houseGrid.ToRealCoordinates(px, py, out vector.x, out vector.y);
-        Gizmos.color = col;
-        Gizmos.DrawSphere(vector, 0.5f);
-    }
+	private void GizmosDrawTile(int px, int py, Color col, GridGraph ggraph)
+	{
+		var vector = Vector2.zero;
+		ggraph.ToRealCoordinates(px, py, out vector.x, out vector.y);
+		Gizmos.color = col;
+		Gizmos.DrawSphere(vector, 0.5f);
+	}
 
     private void OnGUI()
     {
